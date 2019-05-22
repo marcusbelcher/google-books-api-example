1a53a2141edd28a189bae1b483717ada69fd96df
Marcus <marcus.scott.belcher@gmail.com>
2019-05-22 19:36:52 +0100

    Corrected build and moved to /docs

---
+++ docs/precache-manifest.49ce474a518aa89b70b813d3c984f7e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc75668d48c58a805c79cf8bbcf5a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0e12d70df4e99fb5b1",
+    "url": "/static/css/main.d44e8ca6.chunk.css"
+  },
+  {
+    "revision": "17d5d411565bea13e775",
+    "url": "/static/js/2.6cfca651.chunk.js"
+  },
+  {
+    "revision": "ae0e12d70df4e99fb5b1",
+    "url": "/static/js/main.ca03c232.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
