259e08e043166ac7f0999873311b23b8a2282a5e
Marcus <marcus.scott.belcher@gmail.com>
2019-05-22 20:15:56 +0100

    Changed deployment strategy to gh-pages package

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import Layout from '../Layout';
 import theme from './theme';
@@ -8,13 +8,13 @@ import theme from './theme';
 const App: React.FC = (): JSX.Element => {
     return (
         <MuiThemeProvider theme={theme}>
-            <BrowserRouter>
+            <HashRouter>
                 <Layout>
                     <Switch>
                         <Route exact path="/" component={Home} />
                     </Switch>
                 </Layout>
-            </BrowserRouter>
+            </HashRouter>
         </MuiThemeProvider>
     );
 };
